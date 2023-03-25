1033b99a066f9e229eb34121616640049fa56de1
1nary <naritoshi.2714.p3@gmail.com>
2023-03-26 03:24:12 +0900

    add googleフォームURL

--- config/constant.js
+++ config/constant.js
@@ -1,2 +1,3 @@
 export const GITHUB = "https://github.com/1nary" //GitHub URL
 export const TWITTER = "https://twitter.com/1nary_com" //Twitter URL
+export const FORM = "https://forms.gle/JQotQVmyUy6xiswF6"  //Google Form URL
