80a6e09560fbec478b1712fdaf5233ab5bdaef0b
1nary <naritoshi.2714.p3@gmail.com>
2023-04-03 03:43:58 +0900

    add リンク

--- config/constant.js
+++ config/constant.js
@@ -1,3 +1,35 @@
-export const GITHUB = "https://github.com/1nary" //GitHub URL
-export const TWITTER = "https://twitter.com/1nary_com" //Twitter URL
-export const FORM = "https://forms.gle/JQotQVmyUy6xiswF6"  //Google Form URL
+//hrefで指定するURL
+export const HREF = {
+  GITHUB: "https://github.com/1nary",
+  TWITTER: "https://twitter.com/1nary_com",
+  FORM: "https://forms.gle/JQotQVmyUy6xiswF6",
+  COFFEE: "https://www.buymeacoffee.com/naritoshi"
+}
+
+//イメージファイルの場所
+export const IMAGE_SRC = {
+  logo: "/logo.jpg",
+  favicon: "/logo.PNG",
+  twitter: "twitter.svg",
+  twitterWhite: "/twitterWhite.svg",
+  github: "/github.svg",
+  githubWhite: "/githubWhite.svg",
+  css: "/css.svg",
+  html: "/html.svg",
+  figma: "/figma.svg",
+  python: "/python.svg",
+  react: "/react.svg",
+  swift: "/swift.svg",
+  nextjs: "/nextjs.svg",
+  javascript: "/javascript.svg",
+  xd: "/xd.svg",
+  django: "/django.svg",
+  php: "/php.svg",
+  java: "/java.svg",
+  git: "/git.svg",
+  vscode: "/vscode.svg",
+  mysql: "/mysql.svg",
+  slack: "/slack.svg",
+  notion: "/notion.svg",
+  COFFEE: "https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png",
+};
