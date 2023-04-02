8a9ec174c78979ba7f2ee158f3df46c67994074b
1nary <naritoshi.2714.p3@gmail.com>
2023-04-03 03:42:22 +0900

    add 外部リンク

--- next.config.js
+++ next.config.js
@@ -2,5 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
 module.exports = nextConfig
+
+module.exports = {
+  experimental: {
+    optimizeFonts: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.buymeacoffee.com',
+        pathname: '/buttons/v2/default-yellow.png',
+      },
+    ],
+  },
+};
